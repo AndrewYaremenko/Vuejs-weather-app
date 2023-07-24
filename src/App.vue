ad942812481250587b13f8738e187a911c16b27f
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-24 20:58:33 +0300

    styles

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,29 @@
 </script>
 
 <template>
-
+  <div class="wrapper">
+    <h1>Weather App on Vue.js</h1>
+    <p>Check the weather in your city</p>
+  </div>
 </template>
 
 <style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  padding: 20px;
+  border-radius: 50px;
+  border: 2px solid darkblue;
+  background: darkslategray;
+  color: white;
+  text-align: center;
+}
 
+.wrapper h1 {
+  margin-top: 50px;
+}
+
+.wrapper p {
+  margin-top: 20px;
+}
 </style>
